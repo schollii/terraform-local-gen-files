4044c42efec870cc206fdf5a7467dbbeb00e5fa3
Oliver Schoenborn <oliver@sentianSE.com>
2021-03-13 23:36:09 -0500

    fix examples

---
+++ examples/complex/README.md
@@ -0,0 +1,6 @@
+This example shows configuration of a micro-service deployment and 
+testing config files, and defines a helmfile that sets the load order
+of the deployment config files. 
+
+From your
+terraform
--- examples/complex/tf-prod/main.tf
+++ examples/complex/tf-prod/main.tf
@@ -2,13 +2,13 @@ module "gen_helm_values" {
   # source = "git::https://github.com/schollii/terraform-local-gen-files.git"
   source = "../../.."
 
-  stack_id     = "my-stack-1"
+  stack_id     = "prod"
   module_id    = "my-module"
   config_roots = ["../my-micro-svc/deploy-config", "../my-micro-svc/test-config"]
 
   # the keys and values in template_vars are completely arbitrary:
   tmpl_vars = {
-    cluster_name = "my-stack"
+    cluster_name = "prod"
     sg_ingress   = "sg-1234567"
     aws_region   = "us-east-1"
   }
